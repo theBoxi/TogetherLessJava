eb032e07b6125dacf77a327ffd81ac19d2a28f05
theBoxi <online@boxi.ch>
2012-08-12 21:07:49 +0200

    changed register call to HTTP-PUT

--- src/ch/boxi/togetherLess/backend/UserFacade.java
+++ src/ch/boxi/togetherLess/backend/UserFacade.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,7 +19,7 @@ import ch.boxi.togetherLess.dataAccess.UserDAO;
 @Path("/user")
 public class UserFacade {
 	
-	@GET
+	@PUT
 	@Path("register")
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public User register(
