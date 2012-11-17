823e0ad56988903204c89b7eca2c326a584754c1
theBoxi <online@boxi.ch>
2012-11-17 23:49:37 +0100

    Finished JS registration API

--- WEB/register.js
+++ WEB/register.js
@@ -3,27 +3,23 @@ function RegisterAPI(){
 	this.retValue = "";
 	
 	this.register = function(firstName, lastName, userName, password, password2, email){
-		$.ajax({
+		var result = $.ajax({
 		    url: this.host + '/rest/user/register',
 		    type: 'get',
-		    data: "firstName=" +firstName+ "&lastName="+ lastName +"&userName="+ userName +"&password="+ password +"&password2="+ password2+ "&email=" +email, 
-		    success: function(data) { 
-		    	if(data.registrationOK == "true"){
-		    		document.location="registerDone.html";
-		    	} else{
-		    		for(var i = 0; i < data.errors.entry.length; i++){
-		    			if(data.errors.entry[i].value == "true"){
-		    				$("[name="+data.errors.entry[i].key+"]").attr("class", "");
-		    			} else{
-		    				$("[name="+data.errors.entry[i].key+"]").attr("class", "errorInput");
-		    			}
-		    		}
-		    	}
-		    },
-		    error: function(data) { 
-		    	alert('PUT failed\nStatusText:\t ' + data.statusText + '\nresponse:\t ' + data.responseText); 
-		    }
+		    async: false,
+		    data: "firstName=" +firstName+ "&lastName="+ lastName +"&userName="+ userName +"&password="+ password +"&password2="+ password2+ "&email=" +email
 		});
+		
+		if(199 < result.status && result.status < 300){
+			var data = $.parseJSON(result.responseText);
+			if(data.registrationOK == "true"){
+	    		return true;
+	    	} else{
+	    		return data.errors.entry;
+	    	}
+		} else{
+			alert('PUT failed\nStatusText:\t ' + data.statusText + '\nresponse:\t ' + data.responseText);
+		}
 	}
 	
 	this.activate = function(activationCode){
